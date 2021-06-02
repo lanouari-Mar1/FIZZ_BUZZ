6d424a34e096fc35fe4964e334dddd6931d33405
Lanouari Marouane <lanouari.marouane@gmail.com>
2021-06-02 17:36:11 +0200

    [ADD] main vc ui components

--- FizzBuzz/MainViewController.swift
+++ FizzBuzz/MainViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class MainViewController: UIViewController {
   
+  // MARK: Outlets
+  @IBOutlet private weak var scrollView: UIScrollView!
+  @IBOutlet private weak var int1TxtField: UITextField!
+  @IBOutlet private weak var int2TxtField: UITextField!
+  @IBOutlet private weak var limitTxtField: UITextField!
+  @IBOutlet private weak var str1TxtField: UITextField!
+  @IBOutlet private weak var str2TxtField: UITextField!
+  @IBOutlet private weak var resultLbl: UILabel!
+  
+  // MARK: Properties
   private let coordinator: NavigationRoutingLogic
   
+  // MARK: Initializers
   init(coordinator: NavigationRoutingLogic) {
     self.coordinator = coordinator
     super.init(nibName: nil, bundle: nil)
@@ -20,9 +31,14 @@ class MainViewController: UIViewController {
     fatalError("init(coder:) has not been implemented")
   }
   
+  // MARK: UserAction
+  @IBAction private func userDidTapValidate(_ sender: Any) {
+  }
+  
+  // MARK: LifeCycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     // Do any additional setup after loading the view.
   }
 }
