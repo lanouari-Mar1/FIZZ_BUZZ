faa17c8c770cf3efd76b0f915f80fe25edda8604
Lanouari Marouane <lanouari.marouane@gmail.com>
2021-06-02 20:28:47 +0200

    [ADD] displayViewModel struct

---
+++ FizzBuzz/helper/DisplayViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  DisplayViewModel.swift
+//  FizzBuzz
+//
+//  Created by marouane lanouari on 02/06/2021.
+//
+
+import Foundation
+
+/// ViewModel which embded different page state
+struct DisplayViewModel<T> {
+  var context = Observable<DisplayType<T>>(value: .none)
+}
+
+/// Representation of different page state
+enum DisplayType<T> {
+  case isLoading
+  case none
+  case success(T)
+}
