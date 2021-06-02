3bfc7f42b99ca94b26aac6f8727aa8989ed0b77f
Lanouari Marouane <lanouari.marouane@gmail.com>
2021-06-02 20:32:55 +0200

    [ADD] param Model

---
+++ FizzBuzz/model/ParamModel.swift
@@ -0,0 +1,33 @@
+//
+//  ParamModel.swift
+//  FizzBuzz
+//
+//  Created by marouane lanouari on 02/06/2021.
+//
+
+import Foundation
+
+struct CalculModel {
+  
+  let int1: Int
+  let int2: Int
+  let limit: Int
+  let str1: String
+  let str2: String
+  
+  init?(int1Str: String?, int2Str: String?, limitStr: String?, str1: String?, str2: String?) {
+    guard let int1Strong = Int(int1Str ?? ""),
+          let int2Strong = Int(int1Str ?? ""),
+          let limiteStrong = Int(limitStr ?? ""),
+          let str1Strong = str1,
+          let str2Strong = str2 else {
+      return nil
+    }
+    
+    self.int1 = int1Strong
+    self.int2 = int2Strong
+    self.limit = limiteStrong
+    self.str1 = str1Strong
+    self.str2 = str2Strong
+  }
+}
