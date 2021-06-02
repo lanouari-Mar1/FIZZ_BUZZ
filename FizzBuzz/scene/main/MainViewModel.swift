9f4ff5333fab7accad42f4b26ec6a50bbe57eef3
Lanouari Marouane <lanouari.marouane@gmail.com>
2021-06-02 21:46:07 +0200

    [ADD] MainViewModel with displayed result

---
+++ FizzBuzz/scene/main/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  FizzBuzz
+//
+//  Created by marouane lanouari on 02/06/2021.
+//
+
+import Foundation
+
+struct MainViewModel {
+  let displayedResult = DisplayViewModel<String>()
+}
